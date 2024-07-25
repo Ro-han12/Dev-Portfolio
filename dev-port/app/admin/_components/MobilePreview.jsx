5b167e7ba815a2e2fb14b255db1542493aa48b14
rohan <rohannair2939@gmail.com>
2024-07-25 12:37:19 +0530

    preview

--- dev-port/app/admin/_components/MobilePreview.jsx
+++ dev-port/app/admin/_components/MobilePreview.jsx
@@ -1,27 +1,31 @@
+
 "use client";
 
 import { useContext, useEffect, useState } from 'react';
 import { PreviewUpdateContext } from '../../_context/PreviewUpdateContext';
 import { UserDetailContext } from '../../_context/UserDetailContext';
 
-function MobilePreview() {
-  const { updatePreview, setUpdatePreview } = useContext(PreviewUpdateContext);
-  const { userDetail, setUserDetail } = useContext(UserDetailContext);
 
+function MobilePreview() {
+
+  const {updatePreview,setUpdatePreview}=useContext(PreviewUpdateContext)
+  const {userDetail,setUserDetail}=useContext(UserDetailContext)
   return (
-    <div className='p-5 md:fixed'>
-      <div className='border-[13px] lg:w-[330px] xl:w-[350px] w-full border-black max-h-[650px] h-screen rounded-[40px] m-2 shadow-md shadow-primary'>
-        <iframe
-          title='Profile'
-          key={updatePreview}
-          src={`${process.env.NEXT_PUBLIC_BASE_URL}${userDetail?.username}`}
-          width="100%"
-          height="100%"
-          className='rounded-[25px]'
-        />
-      </div>
+    <div className='p-5  md:fixed'>
+        <div className='border-[13px]  lg:w-[330px] xl:w-[350px] w-full border-black 
+        max-h-[650px]
+        h-screen rounded-[40px] m-2 shadow-md shadow-primary'>
+          <iframe
+            title='Profile'
+            key={updatePreview}
+            src={process.env.NEXT_PUBLIC_BASE_URL+userDetail?.username}
+            width="100%"
+            height="100%"
+            className='rounded-[25px]'
+          />
+        </div>
     </div>
-  );
+  )
 }
 
-export default MobilePreview;
+export default MobilePreview
