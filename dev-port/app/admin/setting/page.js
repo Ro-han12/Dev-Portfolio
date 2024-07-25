4e6f5d50afe2ec4656c4eef3e0236a211e5842a2
rohan <rohannair2939@gmail.com>
2024-07-25 21:42:03 +0530

    settings page added

--- dev-port/app/admin/setting/page.js
+++ dev-port/app/admin/setting/page.js
@@ -1,15 +1,11 @@
+import { UserProfile } from '@clerk/nextjs'
 import React from 'react'
 
 function Setting() {
   return (
-    <div className='h-screen p-10'>
-       <h2 className='font-bold text-2xl mt-10'>Settings</h2>
-
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 my-10'>
-        
-       
+    <div className='flex items-center justify-center h-full'>
+      <UserProfile />
       </div>
-    </div>
   )
 }
 
