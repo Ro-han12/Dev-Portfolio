269a8428f11cde162d1f0464234562b7730dba3f
Devashri Nandkumar Bhosale <devashri.bhosale.ai@ghrcem.raisoni.net>
2024-07-25 16:14:00 +0530

    Create page.js

---
+++ dev-port/app/admin/setting/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Setting() {
+  return (
+    <div className='h-screen p-10'>
+       <h2 className='font-bold text-2xl mt-10'>Settings</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 my-10'>
+        
+       
+      </div>
+    </div>
+  )
+}
+
+export default Setting
