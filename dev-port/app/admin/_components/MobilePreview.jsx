9236c9f0146c7d0fd58b68292d64de4c98788ffc
rohan <rohannair2939@gmail.com>
2024-07-25 15:39:31 +0530

    prev

--- dev-port/app/admin/_components/MobilePreview.jsx
+++ dev-port/app/admin/_components/MobilePreview.jsx
@@ -1,30 +1,38 @@
-"use client"
+"use client";
 
-import { useContext, useEffect, useState } from 'react'
-import { PreviewUpdateContext } from '../../_context/PreviewUpdateContext'
-import { UserDetailContext } from '../../_context/UserDetailContext'
-
+import { useContext, useEffect, useState } from 'react';
+import { PreviewUpdateContext } from '../../_context/PreviewUpdateContext';
+import { UserDetailContext } from '../../_context/UserDetailContext';
 
 function MobilePreview() {
+  const { updatePreview } = useContext(PreviewUpdateContext);
+  const { userDetail } = useContext(UserDetailContext);
+  const [iframeUrl, setIframeUrl] = useState('');
 
-  const {updatePreview,setUpdatePreview}=useContext(PreviewUpdateContext)
-  const {userDetail,setUserDetail}=useContext(UserDetailContext)
+  useEffect(() => {
+    if (userDetail && userDetail.username) {
+      setIframeUrl(`${process.env.NEXT_PUBLIC_BASE_URL}/${userDetail.username}`);
+    }
+  }, [userDetail]);
+
   return (
-    <div className='p-5  md:fixed'>
-        <div className='border-[13px]  lg:w-[330px] xl:w-[350px] w-full border-black 
-        max-h-[650px]
-        h-screen rounded-[40px] m-2 shadow-md shadow-primary'>
+    <div className='p-5 md:fixed'>
+      <div className='border-[13px] lg:w-[330px] xl:w-[350px] w-full border-black max-h-[650px] h-screen rounded-[40px] m-2 shadow-md shadow-primary'>
+        {iframeUrl ? (
           <iframe
             title='Profile'
             key={updatePreview}
-            src={process.env.NEXT_PUBLIC_BASE_URL+userDetail?.username}
+            src={iframeUrl}
             width="100%"
             height="100%"
             className='rounded-[25px]'
           />
-        </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MobilePreview
+export default MobilePreview;
